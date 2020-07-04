10e239ca3de30cd93ceeaadfd01b0b61af5392d7
JavierFromNano <javierpernia89@gmail.com>
2020-07-03 22:07:35 -0500

    fist servotest

--- Servo/Servotest.py
+++ Servo/Servotest.py
@@ -11,20 +11,20 @@ import sys
 def sweepTest():
 	sweep = range(0,180)
 	for degree in sweep :
-		kit.servo[0].angle=degree
+		kit.servo[15].angle=degree
     	# kit.servo[1].angle=degree
     	# time.sleep(0.01)
 
 	time.sleep(0.5)
 	sweep = range(180,0, -1)
 	for degree in sweep :
-		kit.servo[0].angle=degree
+		kit.servo[15].angle=degree
 
 # On the Jetson Nano
 # Bus 0 (pins 28,27) is board SCL_1, SDA_1 in the jetson board definition file
 # Bus 1 (pins 5, 3) is board SCL, SDA in the jetson definition file
 # Default is to Bus 1; We are using Bus 0, so we need to construct the busio first ...
-angle = int(sys.argv[1])
+#angle = int(sys.argv[1])
 print("Initializing Servos")
 i2c_bus0=(busio.I2C(board.SCL_1, board.SDA_1))
 print("Initializing ServoKit")
@@ -33,5 +33,5 @@ kit = ServoKit(channels=16, i2c=i2c_bus0)
 # kit[1] is the top servo
 print("Done initializing")
 sweepTest()
-    
+
 
