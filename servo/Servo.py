5e33fb5454ed82c0a296b553ac316413891b911d
Javier Pernia <javierpernia89@gmail.com>
2020-07-06 19:28:00 -0500

    .

--- servo/Servo.py
+++ servo/Servo.py
@@ -24,7 +24,7 @@ class Servo(object):
 	_DEBUG = False
 	_DEBUG_INFO = 'DEBUG "Servo.py":'
 
-	def __init__(self, channel, offset=0, lock=True, bus_number=None, address=0x40):
+	def __init__(self, channel, offset=0, lock=True, bus_number=0, address=0x40):
 		''' Init a servo on specific channel, this offset '''
 		if channel<0 or channel > 16:
 			raise ValueError("Servo channel \"{0}\" is not in (0, 15).".format(channel))
@@ -105,7 +105,7 @@ class Servo(object):
 def test():
 	'''Servo driver test on channel 1'''
 	import time
-	a = Servo(1)
+	a = Servo(15)
 	a.setup()
 	for i in range(0, 180, 5):
 		print(i)
