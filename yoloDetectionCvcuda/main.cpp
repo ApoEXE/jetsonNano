8801b226befd156697a1a9845ce73e5d0f2e8acb
JPWindows_WSL <javierpernia@gmail.com>
2020-07-28 11:48:08 -0500

    added dir access control

--- yoloDetectionCvcuda/main.cpp
+++ yoloDetectionCvcuda/main.cpp
@@ -1,16 +1,27 @@
 //g++ main.cpp dnn_opencv.cpp  -o test  `pkg-config opencv --cflags --libs`
 #include <opencv2/highgui.hpp>
 #include "dnn_opencv.h"
-
+#include <unistd.h>
 std::string absPath_weights = "~/wsl/weights/yolov3-tiny/";
 std::string classes = absPath_weights + "coco.names";
 std::string weights = absPath_weights + "yolov3-tiny.weights";
 std::string conf = absPath_weights + "yolov3-tiny.cfg";
-
+bool FileExists(const std::string &Filename);
 dnn_opencv dnn = dnn_opencv();
 int main(int argc, char const *argv[])
 {
     printf("%s",argv[1]);
+    if (!FileExists(weights) || !FileExists(conf) || !FileExists(argv[1]))
+    {
+        std::cerr << "check which one is missing: " << weights << " " << conf << " " << argv[1] << "\n";
+        exit(0);
+    }
+    else
+    {
+        std::cout << weights << "\n"
+                  << conf << "\n"
+                  << argv[1] << "\n";
+    }
     dnn.load_model(conf, weights, classes);
     cv::VideoCapture cap;
     
@@ -43,4 +54,10 @@ int main(int argc, char const *argv[])
     }
     cap.release();
     return 0;
+}
+
+bool FileExists(const std::string &Filename)
+{
+    bool check = access(Filename.c_str(), 0) == 0;
+    return check;
 }
